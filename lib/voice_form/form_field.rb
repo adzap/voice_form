331df56c99d3c27bd77a08a02c844076de0a6371
Adam Meehan <adam.meehan@gmail.com>
2008-11-11 21:13:50 +1100

    added confirm callback to confirm value entered

--- lib/voice_form/form_field.rb
+++ lib/voice_form/form_field.rb
@@ -3,63 +3,78 @@ module VoiceForm
   class FormField
     attr_reader :name
     attr_accessor :prompts
-    
+
     def initialize(name, options, component)
       @name, @options, @component = name, options, component
       @options.reverse_merge!(:attempts => 5, :call_context => 'call_context')
       @callbacks = {}
       @prompts = []
     end
-    
+
     def prompt(options)
       add_prompts(options.reverse_merge(:timeout => 5))
     end
-    
+
     def reprompt(options)
       raise 'A reprompt can only be used after a prompt' if @prompts.empty?
       add_prompts(options.reverse_merge(:timeout => 5))
     end
-  
+
     def setup(&block)
       @callbacks[:setup] = block
     end
-  
+
     def validate(&block)
       @callbacks[:validate] = block
     end
-    
+
     def invalid(&block)
       @callbacks[:invalid] = block
     end
-    
+
     def timeout(&block)
       @callbacks[:timeout] = block
     end
-    
+
     def success(&block)
       @callbacks[:success] = block
     end
-  
+
     def failure(&block)
       @callbacks[:failure] = block
     end
-        
+
+    def confirm(options={}, &block)
+      options.reverse_merge!(
+        :attempts => 3,
+        :accept   => 1,
+        :reject   => 2,
+        :timeout  => 3
+      )
+      @confirmation_options = options.merge(:block => block)
+    end
+
     def run(component=nil)
       @component = component if component
-            
+
       set_component_value('')
-      
+
       run_callback(:setup)
-      
+
       result = 1.upto(@options[:attempts]) do |attempt|
         if get_input(attempt).empty?
           run_callback(:timeout)
           next
-        end        
+        end
+
         set_component_value @value
-        
-        if validate_input
-          break 0
+
+        if input_valid?
+          if value_confirmed?
+            break 0
+          else
+            next
+          end
         else
           run_callback(:invalid)
         end
@@ -70,7 +85,7 @@ module VoiceForm
         run_callback(:failure)
       end
     end
-   
+
     private
 
     def prompt_for_attempt(attempt)
@@ -79,12 +94,9 @@ module VoiceForm
       else
         @prompts[attempt-1] || @prompts.last
       end
-      key = prompt.has_key?(:play) ? :play : :speak
-      message = prompt[key]
-      message = @component.instance_eval(&message) if message.is_a?(Proc)
-      prompt.merge(key => message)
+      evaluate_prompt(prompt)
     end
-    
+
     def get_input(attempt)
       input_options = @options.dup
       input_options.merge!(prompt_for_attempt(attempt))
@@ -93,13 +105,39 @@ module VoiceForm
       args.unshift(length) if length
       @value = call_context.input(*args)
     end
-    
-    def validate_input
+
+    def input_valid?
       @value.size >= minimum_length &&
         @value.size <= maximum_length &&
         run_callback(:validate)
     end
-    
+
+    def value_confirmed?
+      return true unless @confirmation_options
+      options = @confirmation_options.dup
+
+      if block = options.delete(:block)
+        message = @component.instance_eval(&block)
+        prompt = case message
+        when Array:  {:play  => message}
+        when String: {:speak => message}
+        end
+        prompt.merge!(options.slice(:timeout))
+      end
+      1.upto(options[:attempts]) do |attempt|
+        value = call_context.input(1, prompt)
+        case value
+        when options[:accept].to_s
+          return true
+        when options[:reject].to_s
+          return false
+        else
+          next
+        end
+      end
+      false
+    end
+
     def run_callback(callback)
       if block = @callbacks[callback]
         result = @component.instance_eval(&block)
@@ -107,32 +145,39 @@ module VoiceForm
         result
       else
         true
-      end 
+      end
     end
-   
+
     def set_component_value(value)
       @component.send("#{@name}=", @value)
     end
-    
+
     def get_component_value
       @component.send("#{@name}")
     end
-    
+
     def minimum_length
       @options[:min_length] || @options[:length] || 1
     end
-    
+
     def maximum_length
       @options[:max_length] || @options[:length] || @value.size
     end
-    
+
     def call_context
       @call_context ||= @component.send(@options[:call_context])
     end
-    
+
     def add_prompts(options)
       repeats = options[:repeats] || 1
       @prompts += ([options] * repeats)
+    end
+
+    def evaluate_prompt(prompt)
+      key = prompt.has_key?(:play) ? :play : :speak
+      message = prompt[key]
+      message = @component.instance_eval(&message) if message.is_a?(Proc)
+      prompt.merge(key => message)
     end
   end
 
--- spec/form_field_spec.rb
+++ spec/form_field_spec.rb
@@ -2,45 +2,45 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe VoiceForm::FormField do
   include VoiceForm::FormMethods
-  
+
   attr_accessor :call_context, :my_field
-  
+
   before do
     @call_context = mock('CallContext', :play => nil, :speak => nil)
     @call_context.stub!(:input).with(any_args).and_return('')
   end
-  
+
   it "should define accessor for field in component" do
     field(:my_field) do
       prompt :speak => 'test'
     end
     self.methods.include?(:my_field)
   end
-  
+
   it "should raise error if no prompts defined" do
     item = form_field(:my_field) do
     end
     lambda { item.run }.should raise_error
   end
-  
-  it "should raise error if reprompt defined before prompt" do    
-    lambda { 
+
+  it "should raise error if reprompt defined before prompt" do
+    lambda {
       form_field(:my_field) do
         reprompt :speak => 'and again'
       end
      }.should raise_error
   end
-  
+
   it "should return same prompt for for all attempts if single prompt" do
-    item = form_field(:my_field) do 
+    item = form_field(:my_field) do
       prompt :speak => "first"
     end
     item.send(:prompt_for_attempt, 1)[:speak].should == 'first'
     item.send(:prompt_for_attempt, 2)[:speak].should == 'first'
   end
-  
+
   it "should return reprompt for subsequent prompts" do
-    item = form_field(:my_field) do 
+    item = form_field(:my_field) do
       prompt :speak => "first"
       reprompt :speak => 'next'
     end
@@ -48,9 +48,9 @@ describe VoiceForm::FormField do
     item.send(:prompt_for_attempt, 2)[:speak].should == 'next'
     item.send(:prompt_for_attempt, 3)[:speak].should == 'next'
   end
-  
+
   it "should return prompt for given number of repeats before subsequent prompts" do
-    item = form_field(:my_field) do 
+    item = form_field(:my_field) do
       prompt :speak => "first", :repeats => 2
       reprompt :speak => 'next'
     end
@@ -58,141 +58,217 @@ describe VoiceForm::FormField do
     item.send(:prompt_for_attempt, 2)[:speak].should == 'first'
     item.send(:prompt_for_attempt, 3)[:speak].should == 'next'
   end
-  
+
   it "should set input value in component" do
-    item = form_field(:my_field, :length => 3) do 
+    item = form_field(:my_field, :length => 3) do
       prompt :speak => "first"
     end
     call_context.stub!(:input).and_return('123')
     item.run
-    
+
     my_field.should == '123'
   end
-    
+
   it "should run setup callback once" do
     call_me = i_should_be_called
-    item = form_field(:my_field, :attempts => 3) do 
+    item = form_field(:my_field, :attempts => 3) do
       prompt :speak => "first"
-      
-      setup do
-        call_me.call
-      end
+      setup { call_me.call }
     end
     call_context.should_receive(:input).and_return('')
-    
-    item.run
-  end  
-    
-  it "should run timeout callback if no input" do
-    call_me = i_should_be_called
-    item = form_field(:my_field, :attempts => 1) do 
-      prompt :speak => "first"
-      
-      timeout do
-        call_me.call
-      end
-    end
-    call_context.should_receive(:input).and_return('')
-    
+
     item.run
   end
-  
+
+  it "should run timeout callback if no input" do
+    call_me = i_should_be_called
+    item = form_field(:my_field, :attempts => 1) do
+      prompt :speak => "first"
+      timeout { call_me.call }
+    end
+    call_context.should_receive(:input).and_return('')
+
+    item.run
+  end
+
   it "should make all attempts to get valid input" do
     item = form_field(:my_field) do
       prompt :speak => "first"
     end
     call_context.should_receive(:input).exactly(3).times
-    
+
     item.run
   end
-  
+
   it "should make one attempt if input is valid" do
-    item = form_field(:my_field) do 
+    item = form_field(:my_field) do
       prompt :speak => "first"
     end
-    item.stub!(:validate_input).and_return(true)    
+    item.stub!(:input_valid?).and_return(true)
     call_context.should_receive(:input).once
-    
+
     item.run
   end
-  
-  it "should run validate input" do
-    item = form_field(:my_field, :length => 3) do 
+
+  it "should check if input_valid?" do
+    item = form_field(:my_field, :length => 3) do
       prompt :speak => "first"
-      
     end
     call_context.should_receive(:input).and_return('123')
-    item.should_receive(:validate_input)
-        
+    item.should_receive(:input_valid?)
+
     item.run
   end
-  
-  it "should run validation block if defined" do
+
+  it "should run validation callback if defined" do
     call_me = i_should_be_called
     item = form_field(:my_field, :length => 3, :attempts => 1) do
       prompt :speak => "first"
-      
-      validate do
-        call_me.call
-      end
+      validate { call_me.call }
     end
     call_context.stub!(:input).and_return('123')
-    
+
     item.run
   end
-  
-  it "should run failure if no input" do
+
+  describe "confirm callback" do
+
+    it "should run if defined" do
+      call_me = i_should_be_called
+      item = form_field(:my_field, :length => 3, :attempts => 1) do
+        prompt :speak => "first"
+        confirm { call_me.call; [] }
+      end
+      call_context.stub!(:input).and_return('123')
+
+      item.run
+    end
+
+    it "should not run if not valid input" do
+      dont_call_me = i_should_not_be_called
+      item = form_field(:my_field, :length => 3, :attempts => 1) do
+        prompt :speak => "first"
+        validate { false }
+        confirm { dont_call_me.call }
+      end
+      call_context.stub!(:input).and_return('123')
+
+      item.run
+    end
+
+    it "should be run the number of attempts if no valid response" do
+      call_context.should_receive(:input).with(3, anything).and_return('123')
+      item = form_field(:my_field, :length => 3, :attempts => 1) do
+        prompt :speak => "first"
+
+        confirm(:attempts => 3) { [] }
+      end
+      call_context.should_receive(:input).with(1, anything).exactly(3).times.and_return('')
+
+      item.run
+    end
+
+    it "should run success callback if accept value entered" do
+      call_me = i_should_be_called
+      call_context.should_receive(:input).with(3, anything).and_return('123')
+      item = form_field(:my_field, :length => 3, :attempts => 1) do
+        prompt :speak => "first"
+
+        success { call_me.call }
+        confirm(:accept => '1', :reject => '2') { [] }
+      end
+      call_context.should_receive(:input).with(1, anything).and_return('1')
+
+      item.run
+    end
+
+    it "should run failure callback if reject value entered" do
+      call_me = i_should_be_called
+      call_context.should_receive(:input).with(3, anything).and_return('123')
+      item = form_field(:my_field, :length => 3, :attempts => 1) do
+        prompt :speak => "first"
+
+        failure { call_me.call }
+        confirm(:accept => '1', :reject => '2') { [] }
+      end
+      call_context.should_receive(:input).with(1, anything).and_return('2')
+
+      item.run
+    end
+
+    it "should play confirmation input prompt if confirm block return value is array" do
+      call_me = i_should_be_called
+      call_context.should_receive(:input).with(3, anything).and_return('123')
+
+      item = form_field(:my_field, :length => 3, :attempts => 1) do
+        prompt :speak => "first"
+
+        success { call_me.call }
+        confirm(:timeout => 3) { [] }
+      end
+      call_context.should_receive(:input).with(1, {:play => [], :timeout => 3}).and_return('1')
+
+      item.run
+    end
+
+    it "should speak confirmation input prompt if confirm block return value is string" do
+      call_me = i_should_be_called
+      call_context.should_receive(:input).with(3, anything).and_return('123')
+
+      item = form_field(:my_field, :length => 3, :attempts => 1) do
+        prompt :speak => "first"
+
+        success { call_me.call }
+        confirm(:timeout => 3) { '' }
+      end
+      call_context.should_receive(:input).with(1, {:speak => '', :timeout => 3}).and_return('1')
+
+      item.run
+    end
+  end
+
+  it "should run failure callback if no input" do
     call_me = i_should_be_called
-    item = form_field(:my_field, :length => 3) do 
+    item = form_field(:my_field, :length => 3) do
       prompt :speak => "first"
-        
-      failure do
-        call_me.call
-      end      
+      failure { call_me.call }
     end
     call_context.should_receive(:input).and_return('')
-       
+
     item.run
   end
-  
-  it "should run success if input valid length" do
+
+  it "should run success callback if input valid length" do
     call_me = i_should_be_called
-    item = form_field(:my_field, :length => 3) do 
+    item = form_field(:my_field, :length => 3) do
       prompt :speak => "first"
-        
-      success do
-        call_me.call
-      end      
+      success { call_me.call }
     end
     call_context.should_receive(:input).and_return('123')
-       
+
     item.run
   end
-  
-  it "should run success if input valid length and validates" do
+
+  it "should run success callback if input valid length and valid input" do
     validate_me = i_should_be_called
     call_me = i_should_be_called
-    item = form_field(:my_field, :length => 3) do 
+    item = form_field(:my_field, :length => 3) do
       prompt :speak => "first"
-      
       validate do
         validate_me.call
         my_field.to_i > 100
       end
-        
-      success do
-        call_me.call
-      end
+      success { call_me.call }
     end
     call_context.should_receive(:input).and_return('123')
-       
+
     item.run
   end
-  
+
   def form_field(field, options={}, &block)
     self.class.class_eval { attr_accessor field }
     item = VoiceForm::FormField.new(field, {:attempts => 3}.merge(options), self )
-    
+
     item.instance_eval(&block)
     item
   end
