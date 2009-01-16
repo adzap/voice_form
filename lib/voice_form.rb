f86e3c2399e01c032d8b68349e64c27133902344
Adam Meehan <adam.meehan@gmail.com>
2009-01-16 17:06:03 +1100

    removing old component module inclusion, going back to explicit inclusion in the component

--- lib/voice_form.rb
+++ lib/voice_form.rb
@@ -1,11 +1,3 @@
 require 'voice_form/form_methods'
 require 'voice_form/form'
 require 'voice_form/form_field'
-
-Adhearsion::Components::Behavior.module_eval do
-  include VoiceForm::FormMethods
-end
-
-Adhearsion::Components::Behavior::ClassMethods.module_eval do
-  include VoiceForm::MacroMethods
-end
